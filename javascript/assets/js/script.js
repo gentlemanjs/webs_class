ebab8e6a8d07c5cfb06e59b2de078b5f7cfb3587
gentlemanjs <97070114+gentlemanjs@users.noreply.github.com>
2022-01-18 14:47:25 +0900

    2022.01.18

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -1,25 +1,7 @@
-// 결과보기
+//결과보기 버튼을 클릭하면 경고창을 띄워주세요~
 
-//제목을 클릭했을 때 ~~~를 실행해라!
-// document.querySelector(".result1t h4").addEventListener("click", function(){
-//     document.querySelector(".result1").style.display = "block";
-// })
-
-
-//for문을 이용해서 1~12까지 출력!!
-
-for (let i = 1; i <= 15; i++) {
-    document.querySelector(".result" + i + "t h4").addEventListener("click", function () {
-        //alert("클릭했음");
-        document.querySelector(".result" + i).style.display = "block";
+document.querySelectorAll(".result").forEach(function(el){
+    addEventListener("click", function(){
+        alert("dd");
     });
-}
-
-//forEach문을 이용해서 1~12까지 출력!
-
-document.querySelectorAll(".result h4").forEach(function(el){
-    //console.log(el)
-    el.addEventListener("click", function(){
-        // alert("클릭");
-    })
-});
+})
