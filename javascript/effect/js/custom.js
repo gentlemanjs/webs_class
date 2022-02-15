e4a0f6a284fb1d09be9175558051b3a45caf8612
gentlemanjs <97070114+gentlemanjs@users.noreply.github.com>
2022-02-15 13:13:30 +0900

    2022.02.15

---
+++ javascript/effect/js/custom.js
@@ -0,0 +1,38 @@
+const highlight = function(){
+    // highlight
+    hljs.highlightAll();
+}
+
+// modal
+const modal = function(){
+    document.querySelector(".source-btn").addEventListener("click", () => {
+        document.querySelector("#modal").classList.add("show");
+        document.querySelector("#modal").classList.remove("hide");
+    })
+    document.querySelector(".modal-close").addEventListener("click", () => {
+        document.querySelector("#modal").classList.add("hide");
+    })
+}
+
+// tabmenu
+const tabMenu = function(){
+    const tabBtn = document.querySelectorAll(".view-title > ul > li");
+    const tabCont = document.querySelectorAll(".view-cont > div");
+    
+    // 각각의 버튼을 클릭하면 경고창~~
+    tabBtn.forEach((el, index) => {
+        el.addEventListener("click", ()=>{
+            tabBtn.forEach(btn => {
+                btn.classList.remove("active");
+            });
+            el.classList.add("active");
+
+            // 모든 컨텐츠 박스 안보이게~
+            tabCont.forEach(hide => {
+                hide.style.display = "none"
+            })
+            tabCont[index].style.display = "block"
+            // 내가 클릭한 컨텐츠 박스를 보이게~
+        })
+    })
+}
